746be32f58f417dca75ddebe256155e33efd9062
Priscilla-22 <priscillamw123@gmail.com>
2024-04-08 23:04:50 +0300

    feat:REST Create and Retrieve Lab

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -9,19 +7,45 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        serialized_plants = [plant.serialize() for plant in plants]
+        return make_response(jsonify(serialized_plants), 200)
+
+    def post(self):
+        data = request.get_json()
+        if not data:
+            return make_response(jsonify({"error": "No data provided"}), 400)
+        name = data.get('name')
+        if not name:
+            return make_response(jsonify({"error": "Name is required"}), 400)
+
+        image = data.get('image')
+
+        new_plant = Plant(name=name, image=image, price=data.get('price'))
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.serialize()), 201
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        return jsonify(plant.serialize())
+
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,7 +3,20 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String,nullable=True)
+    price = db.Column(db.Float)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': float(self.price)
+        }
+
